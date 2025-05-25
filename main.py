b43851e5c4e69415456b02f2347dfe95e1212bff
Your Name <your.email@example.com>
2025-05-25 12:16:29 +0530

    new chnages

--- main.py
+++ main.py
@@ -98,5 +98,5 @@ async def startup_event():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", port=8000, reload=True)
-    # uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+    # uvicorn.run("main:app", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
